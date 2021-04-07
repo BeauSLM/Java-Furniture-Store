ff0578bd9f05f0269bf3594528f69dfd6ee90e84
TrueThaumaturge <quentin.jennings@ucalgary.ca>
2021-04-07 16:13:43 -0600

    Added documentation and other minor fixes. Class should be done now. (please display proper name)

--- src/edu/ucalgary/ensf409/DatabaseAccess.java
+++ src/edu/ucalgary/ensf409/DatabaseAccess.java
@@ -6,24 +6,37 @@ import java.util.ArrayList;
  * Class used to access the inventory database.
  */
 public class DatabaseAccess {
-    private Connection dbConnect;
-
+    //SQL Connection stuff
     private final String USERNAME;
     private final String PASSWORD;
     private final String URL;
+    private Connection dbConnect;
 
+    //ArrayLists that store the Database info
     private ArrayList<Manufacturer> manuList;
     private ArrayList<Chair> chairList;
     private ArrayList<Desk> deskList;
     private ArrayList<Lamp> lampList; //ropes, bombs
 
+    /**
+     * Instantiates a new Database access and calls a function to retrieve its data.
+     *
+     * @param username the username
+     * @param password the password
+     * @param url      the url
+     */
     public DatabaseAccess(String username, String password, String url) {
         this.USERNAME = username;
         this.PASSWORD = password;
         this.URL = url;
+        updateDatabaseAccess();
     }
 
-    public void getDatabase() {
+    /**
+     * Creates connection to sql DB, fills the class lists with the database's data.
+     * This calls all the other functions.
+     */
+    public void updateDatabaseAccess() {
         createConnection();
         retrieveManufacturers();
         retrieveChairs();
@@ -32,6 +45,9 @@ public class DatabaseAccess {
         closeConnection();
     }
 
+    /**
+     * Create connection to SQL database.
+     */
     public void createConnection() {
         try {
             dbConnect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
@@ -40,33 +56,42 @@ public class DatabaseAccess {
         }
     }
 
+    /**
+     * Creates an ArrayList of Manufacturers with data from the SQL database.
+     */
     public void retrieveManufacturers() {
         ArrayList<Manufacturer> resultList = new ArrayList<>();
         String Query = "SELECT * FROM Manufacturer";
         ResultSet results;
         try {
             Statement selectAllManus = dbConnect.createStatement();
-            results = selectAllManus.executeQuery(Query);
-            while(results.next()) {
+            results = selectAllManus.executeQuery(Query); //query returned at this point
+            while(results.next()) { //for each result:
+                //add a Manufacturer object to the list
                 resultList.add(new Manufacturer(results.getString("ManuID"), results.getString("name"),
                             results.getString("Phone"), results.getString("Province")));
             }
+            //close it out
             selectAllManus.close();
             results.close();
         } catch(SQLException ex) {
             ex.printStackTrace();
         }
-        manuList = resultList;
+        manuList = resultList; //update field
     }
 
+    /**
+     * Creates an ArrayList of Chairs with data from the SQL database.
+     */
     public void retrieveChairs() {
         ArrayList<Chair> resultList = new ArrayList<>();
         String query = "SELECT * FROM Chair";
         try {
             Statement selectAllChairs = dbConnect.createStatement();
-            ResultSet results = selectAllChairs.executeQuery(query);
+            ResultSet results = selectAllChairs.executeQuery(query); //query returned by this point
 
-            while (results.next()) {
+            while (results.next()) { //for each result:
+                //converts "Y"/"N" to boolean
                 boolean hasLegs = false, hasArms = false, hasSeat = false, hasCushion = false;
                 if (results.getString("Leg").equals("Y")) {
                     hasLegs = true;
@@ -80,26 +105,32 @@ public class DatabaseAccess {
                 if (results.getString("Cushion").equals("Y")) {
                     hasCushion = true;
                 }
+                //creates a Chair object and adds to list
                 resultList.add(new Chair(results.getString("ID"), results.getString("Type"),
                         results.getInt("Price"), results.getString("ManuID"),
                         hasLegs, hasArms, hasSeat, hasCushion));
             }
+            //done! just close stuff now.
             selectAllChairs.close();
             results.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        chairList = resultList;
+        chairList = resultList; //updates field
     }
 
+    /**
+     * Creates an ArrayList of Desks with data from the SQL database.
+     */
     public void retrieveDesks() {
         ArrayList<Desk> resultList = new ArrayList<>();
-        String Query = "SELECT * FROM Desk";
+        String query = "SELECT * FROM Desk";
         try {
             Statement selectAllDesks = dbConnect.createStatement();
-            ResultSet results = selectAllDesks.executeQuery(Query);
+            ResultSet results = selectAllDesks.executeQuery(query); //query returned at this point
 
-            while(results.next()) {
+            while(results.next()) { //for each result:
+                //converts "Y"/"N" to boolean
                 boolean usableLegs = false, usableTop = false, usableDrawer = false;
                 if(results.getString("Legs").equals("Y")) {
                     usableLegs = true;
@@ -110,25 +141,31 @@ public class DatabaseAccess {
                 if(results.getString("Drawer").equals("Y")) {
                     usableDrawer = true;
                 }
+                //creates Desk object and adds to list
                 resultList.add(new Desk(results.getString("ID"), results.getString("Type"),
                         results.getInt("Price"), results.getString("ManuID"),
                         usableLegs, usableTop, usableDrawer));
             }
+            //close em up!
             selectAllDesks.close();
             results.close();
         } catch(SQLException ex) {
             ex.printStackTrace();
         }
-        deskList = resultList;
+        deskList = resultList; //updates field
     }
 
+    /**
+     * Creates an ArrayList of Lamps with data from the SQL database.
+     */
     public void retrieveLamps() {
         ArrayList<Lamp> resultList = new ArrayList<>();
-        String Query = "SELECT * FROM LAMP";
+        String query = "SELECT * FROM Lamp";
         try {
             Statement selectAllLamps = dbConnect.createStatement();
-            ResultSet results = selectAllLamps.executeQuery(Query);
-            while(results.next()) {
+            ResultSet results = selectAllLamps.executeQuery(query); //query returned at this point
+            while(results.next()) { //for each result:
+                //converts "Y"/"N" to boolean
                 boolean usableBase = false, usableBulb = false;
                 if(results.getString("Base").equals("Y")) {
                     usableBase = true;
@@ -136,17 +173,23 @@ public class DatabaseAccess {
                 if(results.getString("Bulb").equals("Y")) {
                     usableBulb = true;
                 }
+                //create and add Lamp object to the list
                 resultList.add(new Lamp(results.getString("ID"), results.getString("Type"),
                         results.getInt("Price"), results.getString("manuID"),
                         usableBase, usableBulb));
             }
+            //close stuff out!
             selectAllLamps.close();
             results.close();
         } catch(SQLException ex) {
             ex.printStackTrace();
         }
-        lampList = resultList;
+        lampList = resultList; //updates field
     }
+
+    /**
+     * Closes the sql connection.
+     */
     public void closeConnection() {
         try {
             dbConnect.close();
