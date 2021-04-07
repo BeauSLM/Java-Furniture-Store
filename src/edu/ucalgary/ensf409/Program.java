0ed669a4ddd30bb12463b8f8d14948d6d16cecec
Michael Corleone <32565641+MichaelDonCorleone@users.noreply.github.com>
2021-04-07 12:13:28 -0600

    Program database access update

--- src/edu/ucalgary/ensf409/Program.java
+++ src/edu/ucalgary/ensf409/Program.java
@@ -10,6 +10,10 @@ public class Program {
      * @param args the input arguments
      */
     public static void main(String[] args) {
-
+        String username = "ENSF409";
+        String password = "ensf409";
+        String URL = "jdbc:mysql://localhost/inventory";
+        DatabaseAccess dbAccess = new DatabaseAccess(username, password, URL);
     }
 }
+
