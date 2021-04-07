13480807a389e65be45a9f2fc07fc31572b6d0d3
Michael Corleone <32565641+MichaelDonCorleone@users.noreply.github.com>
2021-04-07 09:25:32 -0600

    Adding the java files needed to implement furniture objects

--- src/edu/ucalgary/ensf409/DatabaseAccess.java
+++ src/edu/ucalgary/ensf409/DatabaseAccess.java
@@ -1,7 +1,31 @@
 package edu.ucalgary.ensf409;
+import java.sql.*;
 
 /**
  * Class to access the inventory db.
  */
 public class DatabaseAccess {
+    private Connection dbConnect;
+    private ResultSet results;
+    String username;
+    String password;
+    String URL;
+
+    public DatabaseAccess(String username, String password, String URL) {
+        this.username = username;
+        this.password = password;
+        this.URL = URL;
+    }
+
+    public void createConnection(String username, String password, String URL) {
+        try {
+            dbConnect = DriverManager.getConnection(URL, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void selectFurniture() {
+
+    }
 }
