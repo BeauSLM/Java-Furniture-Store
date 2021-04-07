a30cc63816ae20c264b50098a91de4d0d5da9bb0
Beau <mccartney.bsledu@gmail.com>
2021-04-07 12:26:58 -0600

    Added UnitTest class, and an exception for when an item isn't found.

---
+++ src/edu/ucalgary/ensf409/UnitTest.java
@@ -0,0 +1,16 @@
+package edu.ucalgary.ensf409;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Class for unit tests.
+ */
+public class UnitTest {
+
+    /**
+     * Instantiates a new Unit test.
+     */
+    public UnitTest() {
+    }
+}
